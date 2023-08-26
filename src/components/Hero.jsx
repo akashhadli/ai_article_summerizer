15b92e90011c69826cc3dc31529550d5b88dc27f
Aakash Hadli <aakashhadli@gmail.com>
2023-08-26 19:36:01 +0530

    getting summary from api

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,12 @@ const Hero = () => {
 		<header className='w-full flex justify-center items-center flex-col'>
 			<nav className='flex justify-between items-center w-full mb-10 pt-3'>
 				<img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+
 				<button
 					type='button'
-					onClick={() => window.open('https://github.com/akashhadli')}
+					onClick={() =>
+						window.open('https://github.com/TidbitsJS/Summize', '_blank')
+					}
 					className='black_btn'
 				>
 					GitHub
@@ -15,13 +18,12 @@ const Hero = () => {
 			</nav>
 
 			<h1 className='head_text'>
-				Summerize Articles with <br className='max-md:hidden' />
-				<span className='orange_gradient'>OpenAI GPT-4</span>
+				Summarize Articles with <br className='max-md:hidden' />
+				<span className='orange_gradient '>OpenAI GPT-4</span>
 			</h1>
-
 			<h2 className='desc'>
-				Simplify your reading with SumZ, an open-source article summerizer that
-				transforms lengthy articles into clear and concise summaries.
+				Simplify your reading with Summize, an open-source article summarizer
+				that transforms lengthy articles into clear and concise summaries
 			</h2>
 		</header>
 	);
